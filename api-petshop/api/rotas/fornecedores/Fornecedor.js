5fd99159bd2f8d8b8dae548430016dae6c398750
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-16 19:36:36 -0300

    Add update route to fornecedores

--- api-petshop/api/rotas/fornecedores/Fornecedor.js
+++ api-petshop/api/rotas/fornecedores/Fornecedor.js
@@ -35,6 +35,25 @@ class Fornecedor {
         this.versao = fornecedorEncontrado.versao
     }
 
+    async atualizar(){
+        const fornecedor = await TabelaFornecedor.pegarPorId(this.id)
+        const campos = ['empresa', 'email', 'categoria']
+        const dadosParaAtualizar = {}
+
+        campos.forEach((campo) => {
+            const valor = this[campo]
+
+            if (typeof valor === 'string' && valor.length > 0){
+                dadosParaAtualizar[campo] = valor
+            }
+        })
+
+        if(Object.keys(dadosParaAtualizar).length === 0){
+            throw new Error('Não foram fornecidos dados para atualizar!')
+        }
+
+        await TabelaFornecedor.atualizar(this.id, dadosParaAtualizar)
+    }
 }
 
 module.exports = Fornecedor
