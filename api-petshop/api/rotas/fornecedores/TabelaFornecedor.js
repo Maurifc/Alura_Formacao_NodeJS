8ae2edd7af4e48807a8288d4511fe908ea731a7a
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-16 19:17:59 -0300

    Add new routes - create Fornecedor and get Fornecedor (by id)
    - Add Fornecedor class with insert and load methods
    - Add new routes
    - Add methods insert and getById on TabelaFornecedor

--- api-petshop/api/rotas/fornecedores/TabelaFornecedor.js
+++ api-petshop/api/rotas/fornecedores/TabelaFornecedor.js
@@ -3,5 +3,23 @@ const Modelo = require('./ModeloTabelaFornecedor')
 module.exports = {
     listar (){
         return Modelo.findAll() // Returns all rows in table fornecedores
+    },
+
+    inserir(fornecedor){
+        return Modelo.create(fornecedor)
+    },
+
+    async pegarPorId(id){
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if (!encontrado){
+            throw new Error('Fornecedor não encontrado')
+        }
+
+        return encontrado
     }
 }
--- api-petshop/api/rotas/fornecedores/index.js
+++ api-petshop/api/rotas/fornecedores/index.js
@@ -1,12 +1,37 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor') // Syntax sugar to ORM methods
+const Fornecedor = require('./Fornecedor')
 
 // Route /api/fornecedores
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar() // Same as model.findAll()
     res.send(
         JSON.stringify(resultados) // Turn list into json
     )
+})
+
+roteador.post('/', async (req, res) => {
+    const dadosRecebidos = req.body
+    const fornecedores = new Fornecedor(dadosRecebidos)
+
+    await fornecedores.criar()
+
+    res.send(JSON.stringify(fornecedores))
+} )
+
+
+roteador.get('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+    
+        res.send(JSON.stringify(fornecedor))
+    } catch (error) {
+        res.send(JSON.stringify({
+            mensagem: error.message
+        }))    
+    }
 })
 
 module.exports = roteador
