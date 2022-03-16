4276d9175f6e908954e0a9a1ef9eefce0382947b
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-16 18:30:09 -0300

    Add comment and move app const declaration to another line

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const config = require('config')
 const roteador = require('./rotas/fornecedores')
+const app = express()
 
-app.use(bodyParser.json()) //  "Plugin"
+app.use(bodyParser.json()) //  "Express Plugin"
 
-// Routes
+// Declare a route and specify a router for it
 app.use('/api/fornecedores', roteador)
 
 // App start
