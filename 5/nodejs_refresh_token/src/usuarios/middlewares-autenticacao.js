e99545763b2b90cee6cbb5efc742b7f8c03dcc29
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-29 20:57:54 -0300

    add refresh route

--- 5/nodejs_refresh_token/src/usuarios/middlewares-autenticacao.js
+++ 5/nodejs_refresh_token/src/usuarios/middlewares-autenticacao.js
@@ -1,4 +1,27 @@
 const passport = require('passport');
+const { InvalidArgumentError } = require('../erros')
+const allowlistRefreshToken = require('../../redis/allowlist-refresh-token')
+const Usuario = require('./usuarios-modelo')
+
+async function verificarRefreshToken(refreshToken){
+  if(!refreshToken)
+    throw new InvalidArgumentError('Refresh token não enviado!')
+  
+  const id = await allowlistRefreshToken.buscaValor(refreshToken)     // Get user id from refresh token (key)
+  
+  if(!id){                                                            // Check if refresh token is valid (key returned a valid user id)
+    throw new InvalidArgumentError('Refresh token inválido')
+  }
+
+  return id
+}
+
+async function invalidaRefreshToken(refreshToken){
+  if(!refreshToken)
+    throw new InvalidArgumentError('Refresh token não enviado!')
+
+  await allowlistRefreshToken.deleta(refreshToken)
+}
 
 module.exports = {
   local(req, res, next) {
@@ -53,4 +76,19 @@ module.exports = {
       }
     )(req, res, next);
   },
+
+  async refresh(req, res, next) {
+    try {
+      const { refreshToken } = req.body                         // Get refresh token sent through body
+      const id = await verificarRefreshToken(refreshToken)      // Check if is valid and returns user id
+      invalidaRefreshToken(refreshToken)                        // Prevent token use another time
+      req.user = await Usuario.buscaPorId(id)                   // Inject user object on request
+      return next()      
+    } catch (error) {
+      if (error.name === 'InvalidArgumentError')
+        return res.status(401).send({ erro: error.message })
+      
+      return res.status(500).send({ erro: error.message})
+    }
+  }
 };
